12d76675ad6e0e70bb64aea94d705e0b220dacc9
Mulayam-coder <yadavm976@gmail.com>
2020-08-26 22:07:06 +0530

    first

---
+++ Assignment2/author.js
@@ -0,0 +1,17 @@
+let author=new Object(); 
+
+author.id="212";
+author.name= "Tim";
+author.biography="Tim is a poet, writer, and author of the new novel We Were Already There.";
+author.birthday="10/04/1995";
+author.email="tim976@gmail.com";
+author.photographUrl="http//photography.com";
+author.listOfBook="1. War, 2. Independent, 3. Invester ";
+
+console.log(author.id);
+console.log(author.name);
+console.log(author.biography);
+console.log(author.birthday);
+console.log(author.email);
+console.log(author.photographUrl);
+console.log(author.listOfBook);
